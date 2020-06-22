5692d9203180b70625c11e13337dcfadcebde429
“Thepworth95” <56401451+Thepworth95@users.noreply.github.com>
2020-06-22 13:25:29 +0100

    Delete other expenses parser and validator

---
+++ test/v1/controllers/requestParsers/DeleteOtherExpensesRequestParserSpec.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers.requestParsers
+
+import support.UnitSpec
+import uk.gov.hmrc.domain.Nino
+import v1.mocks.validators.MockDeleteOtherExpensesValidator
+import v1.models.errors.{BadRequestError, ErrorWrapper, NinoFormatError, TaxYearFormatError}
+import v1.models.request.deleteOtherExpenses.{DeleteOtherExpensesRawData, DeleteOtherExpensesRequest}
+
+class DeleteOtherExpensesRequestParserSpec extends UnitSpec {
+  val nino = "AA123456B"
+  val taxYear = "2019-20"
+
+  val inputData = DeleteOtherExpensesRawData(nino, taxYear)
+
+  trait Test extends MockDeleteOtherExpensesValidator {
+    lazy val parser = new DeleteOtherExpensesRequestParser(mockValidator)
+  }
+
+  "parse" should {
+
+    "return a request object" when {
+      "valid request data is supplied" in new Test {
+        MockDeleteOtherExpensesValidator.validate(inputData).returns(Nil)
+
+        parser.parseRequest(inputData) shouldBe
+          Right(DeleteOtherExpensesRequest(Nino(nino), "2019-20"))
+      }
+    }
+
+    "return an ErrorWrapper" when {
+
+      "a single validation error occurs" in new Test {
+        MockDeleteOtherExpensesValidator.validate(inputData)
+          .returns(List(NinoFormatError))
+
+        parser.parseRequest(inputData) shouldBe
+          Left(ErrorWrapper(None, NinoFormatError, None))
+      }
+
+      "multiple validation errors occur" in new Test {
+        MockDeleteOtherExpensesValidator.validate(inputData)
+          .returns(List(NinoFormatError, TaxYearFormatError))
+
+        parser.parseRequest(inputData) shouldBe
+          Left(ErrorWrapper(None, BadRequestError, Some(Seq(NinoFormatError, TaxYearFormatError))))
+      }
+    }
+  }
+}
+
