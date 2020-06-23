fe4aca96c0851bc5bd7d150e996de0a524de19f0
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2020-06-23 08:51:29 +0100

    Resolving comments

--- test/v1/controllers/RetrieveOtherExpensesControllerSpec.scala
+++ test/v1/controllers/RetrieveOtherExpensesControllerSpec.scala
@@ -143,7 +143,6 @@ class RetrieveOtherExpensesControllerSpec
         val input = Seq(
           (NinoFormatError, BAD_REQUEST),
           (TaxYearFormatError, BAD_REQUEST),
-          (RuleTaxYearRangeInvalidError, BAD_REQUEST),
           (NotFoundError, NOT_FOUND),
           (DownstreamError, INTERNAL_SERVER_ERROR)
         )
