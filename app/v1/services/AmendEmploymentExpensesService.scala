c6fe383c08eaf641e9eab3edf05c2cf5c68f02ee
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2020-07-30 13:11:21 +0100

    Skeleton for Controller and service

--- app/v1/services/AmendEmploymentExpensesService.scala
+++ app/v1/services/AmendEmploymentExpensesService.scala
@@ -1,5 +1,55 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package v1.services
 
-class AmendEmploymentExpensesService {
+import cats.data.EitherT
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v1.connectors.AmendEmploymentExpensesConnector
+import v1.controllers.EndpointLogContext
+import v1.models.errors.{DownstreamError, NinoFormatError, NotFoundError, RuleTaxYearNotEndedError}
+import v1.support.DesResponseMappingSupport
 
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class AmendEmploymentExpensesService @Inject()(connector: AmendEmploymentExpensesConnector) extends DesResponseMappingSupport with Logging {
+
+  def amend(request: AmendEmploymentExpensesRequest)(
+    implicit hc: HeaderCarrier,
+    ec: ExecutionContext,
+    logContext: EndpointLogContext): Future[AmendEmploymentExpensesServiceOutcome] = {
+
+    val result = for {
+      desResponseWrapper <- EitherT(connector.amend(request)).leftMap(mapDesErrors(desErrorMap))
+    } yield desResponseWrapper
+
+    result.value
+  }
+
+  private def desErrorMap =
+    Map(
+      "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+      "INVALID_TAX_YEAR" -> NotFoundError,
+      "INVALID_CORRELATIONID" -> DownstreamError,
+      "INVALID_PAYLOAD" -> DownstreamError,
+      "INVALID_REQUEST_BEFORE_TAX_YEAR_END" -> RuleTaxYearNotEndedError,
+      "SERVER_ERROR" -> DownstreamError,
+      "SERVICE_UNAVAILABLE" -> DownstreamError
+    )
 }
--- test/v1/connectors/AmendEmploymentExpensesConnectorSpec.scala
+++ test/v1/connectors/AmendEmploymentExpensesConnectorSpec.scala
@@ -1,5 +1,55 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package v1.connectors
 
-class AmendEmploymentExpensesConnectorSpec {
+import uk.gov.hmrc.domain.Nino
 
+class AmendEmploymentExpensesConnectorSpec extends ConnectorSpec {
+
+  val taxYear = "2021-22"
+  val nino = Nino("AA123456A")
+  val body = AmendEmploymentsExpensesBody(
+    Some(PaymentsToTradeUnionsForDeathBenefits(Some("TRADE UNION PAYMENTS"), 2000.99)),
+    Some(PatentRoyaltiesPayments(Some("ROYALTIES PAYMENTS"), 2000.99))
+  )
+
+  class Test extends MockHttpClient with MockAppConfig {
+    val connector: AmendOtherExpensesConnector = new AmendOtherExpensesConnector(http = mockHttpClient, appConfig = mockAppConfig)
+
+    val desRequestHeaders: Seq[(String, String)] = Seq("Environment" -> "des-environment", "Authorization" -> s"Bearer des-token")
+    MockedAppConfig.desBaseUrl returns baseUrl
+    MockedAppConfig.desToken returns "des-token"
+    MockedAppConfig.desEnvironment returns "des-environment"
+  }
+
+  "amend" should {
+    val request = AmendOtherExpensesRequest(nino, taxYear, body)
+
+    "put a body and return 204 no body" in new Test {
+      val outcome = Right(ResponseWrapper(correlationId, ()))
+      MockedHttpClient
+        .put(
+          url = s"$baseUrl/expenses/other/$nino/$taxYear",
+          body = body,
+          requiredHeaders = "Environment" -> "des-environment", "Authorization" -> s"Bearer des-token"
+        )
+        .returns(Future.successful(outcome))
+
+      await(connector.amend(request)) shouldBe outcome
+    }
+  }
 }
