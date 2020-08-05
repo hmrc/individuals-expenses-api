a5b369945ba45573e1ba2c796b36059f1f46ab48
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2020-08-05 15:57:01 +0100

    MTDSA-6587 Initial Skeleton before Models rebase

--- app/v1/controllers/RetrieveEmploymentExpensesController.scala
+++ app/v1/controllers/RetrieveEmploymentExpensesController.scala
@@ -16,8 +16,59 @@
 
 package v1.controllers
 
-import javax.inject.Inject
+import cats.data.EitherT
+import javax.inject.{Inject, Singleton}
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, ControllerComponents}
+import utils.Logging
+import v1.hateoas.HateoasFactory
+import v1.models.errors.{BadRequestError, DownstreamError, ErrorWrapper, NinoFormatError, NotFoundError, RuleTaxYearRangeInvalidError, TaxYearFormatError}
+import v1.services.{EnrolmentsAuthService, MtdIdLookupService}
 
-class RetrieveEmploymentExpensesController @Inject()() {
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class RetrieveEmploymentExpensesController @Inject()(val authService: EnrolmentsAuthService,
+                                                     val lookupService: MtdIdLookupService,
+                                                     parser: RetrieveEmploymentExpensesRequestParser,
+                                                     service: RetrieveEmploymentExpensesService,
+                                                     hateoasFactory: HateoasFactory,
+                                                     cc: ControllerComponents)(implicit ec: ExecutionContext)
+  extends AuthorisedController(cc) with BaseController with Logging {
+
+  implicit val endpointLogContext: EndpointLogContext =
+    EndpointLogContext(controllerName = "RetrieveEmploymentExpensesController", endpointName = "retrieveEmploymentExpenses")
+
+  def handleRequest(nino: String, taxYear: String): Action[AnyContent] =
+    authorisedAction(nino).async { implicit request =>
+      val rawData = RetrieveEmploymentExpensesRawData(nino, taxYear)
+      val result =
+        for {
+          parsedRequest <- EitherT.fromEither[Future](parser.parseRequest(rawData))
+          serviceResponse <- EitherT(service.retrieveEmploymentExpenses(parsedRequest))
+          vendorResponse <- EitherT.fromEither[Future](
+            hateoasFactory.wrap(serviceResponse.responseData, RetrieveEmploymentExpensesHateoasData(nino, taxYear)).asRight[ErrorWrapper])
+        } yield {
+          logger.info(
+            s"[${endpointLogContext.controllerName}][${endpointLogContext.endpointName}] - " +
+              s"Success response received with CorrelationId: ${serviceResponse.correlationId}")
+
+          Ok(Json.toJson(vendorResponse))
+            .withApiHeaders(serviceResponse.correlationId)
+        }
+
+      result.leftMap { errorWrapper =>
+        val correlationId = getCorrelationId(errorWrapper)
+        errorResult(errorWrapper).withApiHeaders(correlationId)
+      }.merge
+    }
+
+  private def errorResult(errorWrapper: ErrorWrapper) = {
+    (errorWrapper.error: @unchecked) match {
+      case NinoFormatError | BadRequestError | TaxYearFormatError | RuleTaxYearRangeInvalidError => BadRequest(Json.toJson(errorWrapper))
+      case DownstreamError => InternalServerError(Json.toJson(errorWrapper))
+      case NotFoundError => NotFound(Json.toJson(errorWrapper))
+    }
+  }
 
 }
---
+++ test/v1/controllers/RetrieveEmploymentExpensesControllerSpec.scala
@@ -0,0 +1,152 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.controllers
+
+import play.api.libs.json.Json
+import play.api.mvc.Result
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.mocks.hateoas.MockHateoasFactory
+import v1.mocks.services.{MockAuditService, MockEnrolmentsAuthService, MockMtdIdLookupService}
+import v1.models.errors.{BadRequestError, DownstreamError, ErrorWrapper, MtdError, NinoFormatError, NotFoundError, RuleTaxYearRangeInvalidError, TaxYearFormatError}
+import v1.models.hateoas.{HateoasWrapper, Link}
+import v1.models.hateoas.Method.GET
+import v1.models.outcomes.ResponseWrapper
+
+import scala.concurrent.Future
+
+class RetrieveEmploymentExpensesControllerSpec
+  extends ControllerBaseSpec
+  with MockEnrolmentsAuthService
+  with MockMtdIdLookupService
+  with MockRetrieveEmploymentExpensesService
+  with MockRetrieveEmploymentExpensesRequestParser
+  with MockHateoasFactory
+  with MockAuditService {
+
+
+  trait Test {
+    val hc = HeaderCarrier()
+
+    val controller = new RetrieveOtherExpensesController(
+      authService = mockEnrolmentsAuthService,
+      lookupService = mockMtdIdLookupService,
+      parser = mockRetrieveEmploymentExpensesRequestParser,
+      service = mockRetrieveEmploymentExpensesService,
+      hateoasFactory = mockHateoasFactory,
+      cc = cc,
+    )
+
+    MockedMtdIdLookupService.lookup(nino).returns(Future.successful(Right("test-mtd-id")))
+    MockedEnrolmentsAuthService.authoriseUser()
+  }
+
+  private val nino = "AA123456A"
+  private val taxYear = "2019-20"
+  private val source = "latest"
+  private val correlationId = "X-123"
+
+  private val rawData = RetrieveEmploymentExpensesRawData(nino, taxYear, source)
+  private val requestData = RetrieveEmploymentExpensesRequest(Nino(nino), taxYear, source)
+
+  private val testHateoasLink = Link(href = s"individuals/expenses/employment/$nino/$taxYear?source=$source", method = GET, rel = "self")
+
+  private val responseBody = RetrieveEmploymentExpensesBody(
+    Some(PaymentsToTradeUnionsForDeathBenefits(Some("TRADE UNION PAYMENTS"), 4528.99)),
+    Some(PatentRoyaltiesPayments(Some("ROYALTIES PAYMENTS"), 2000.10))
+  )
+
+  "handleRequest" should {
+    "return Ok" when {
+      "the request received is valid" in new Test {
+
+        MockRetrieveOtherExpensesRequestParser
+          .parse(rawData)
+          .returns(Right(requestData))
+
+        MockRetrieveOtherExpensesService
+          .retrieveOtherExpenses(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, responseBody))))
+
+        MockHateoasFactory
+          .wrap(responseBody, RetrieveOtherExpensesHateoasData(nino, taxYear))
+          .returns(HateoasWrapper(responseBody, Seq(testHateoasLink)))
+
+        val result: Future[Result] = controller.handleRequest(nino, taxYear)(fakeRequest)
+        status(result) shouldBe OK
+        header("X-CorrelationId", result) shouldBe Some(correlationId)
+      }
+    }
+    "return the error as per spec" when {
+      "parser errors occur" should {
+        def errorsFromParserTester(error: MtdError, expectedStatus: Int): Unit = {
+          s"a ${error.code} error is returned from the parser" in new Test {
+
+            MockRetrieveOtherExpensesRequestParser
+              .parse(rawData)
+              .returns(Left(ErrorWrapper(Some(correlationId), error, None)))
+
+            val result: Future[Result] = controller.handleRequest(nino, taxYear)(fakeRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(error)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (BadRequestError, BAD_REQUEST),
+          (NinoFormatError, BAD_REQUEST),
+          (TaxYearFormatError, BAD_REQUEST),
+          (RuleTaxYearRangeInvalidError, BAD_REQUEST)
+        )
+
+        input.foreach(args => (errorsFromParserTester _).tupled(args))
+      }
+
+      "service errors occur" should {
+        def serviceErrors(mtdError: MtdError, expectedStatus: Int): Unit = {
+          s"a $mtdError error is returned from the service" in new Test {
+
+            MockRetrieveOtherExpensesRequestParser
+              .parse(rawData)
+              .returns(Right(requestData))
+
+            MockRetrieveOtherExpensesService
+              .retrieveOtherExpenses(requestData)
+              .returns(Future.successful(Left(ErrorWrapper(Some(correlationId), mtdError))))
+
+            val result: Future[Result] = controller.handleRequest(nino, taxYear)(fakeRequest)
+
+            status(result) shouldBe expectedStatus
+            contentAsJson(result) shouldBe Json.toJson(mtdError)
+            header("X-CorrelationId", result) shouldBe Some(correlationId)
+          }
+        }
+
+        val input = Seq(
+          (NinoFormatError, BAD_REQUEST),
+          (TaxYearFormatError, BAD_REQUEST),
+          (NotFoundError, NOT_FOUND),
+          (DownstreamError, INTERNAL_SERVER_ERROR)
+        )
+
+        input.foreach(args => (serviceErrors _).tupled(args))
+      }
+    }
+  }
+}
