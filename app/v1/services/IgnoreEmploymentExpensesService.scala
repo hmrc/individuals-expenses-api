12238f8e836d0ac2b07cf97076aa37c65b0b9609
Mbrookesy <56401421+Mbrookesy@users.noreply.github.com>
2020-08-05 08:57:21 +0100

    MTDSA-6563 Ignore Employment Expenses Connector & Service (#39)

---
+++ app/v1/services/IgnoreEmploymentExpensesService.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import cats.data.EitherT
+import cats.implicits._
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.http.HeaderCarrier
+import utils.Logging
+import v1.connectors.IgnoreEmploymentExpensesConnector
+import v1.controllers.EndpointLogContext
+import v1.models.errors.{DownstreamError, NinoFormatError, NotFoundError, RuleTaxYearNotEndedError}
+import v1.models.request.ignoreEmploymentExpenses.IgnoreEmploymentExpensesRequest
+import v1.support.DesResponseMappingSupport
+
+import scala.concurrent.{ExecutionContext, Future}
+
+@Singleton
+class IgnoreEmploymentExpensesService @Inject()(connector: IgnoreEmploymentExpensesConnector) extends DesResponseMappingSupport with Logging {
+
+  def amend(request: IgnoreEmploymentExpensesRequest)(
+    implicit hc: HeaderCarrier,
+    ec: ExecutionContext,
+    logContext: EndpointLogContext): Future[IgnoreEmploymentExpensesServiceOutcome] = {
+
+    val result = for {
+      desResponseWrapper <- EitherT(connector.ignore(request)).leftMap(mapDesErrors(desErrorMap))
+    } yield desResponseWrapper
+
+    result.value
+  }
+
+  private def desErrorMap =
+    Map(
+      "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+      "INVALID_TAX_YEAR" -> NotFoundError,
+      "INVALID_CORRELATIONID" -> DownstreamError,
+      "INVALID_PAYLOAD" -> DownstreamError,
+      "INVALID_REQUEST_BEFORE_TAX_YEAR_END" -> RuleTaxYearNotEndedError,
+      "SERVER_ERROR" -> DownstreamError,
+      "SERVICE_UNAVAILABLE" -> DownstreamError
+    )
+}
---
+++ test/v1/services/IgnoreEmploymentExpensesServiceSpec.scala
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.services
+
+import support.UnitSpec
+import uk.gov.hmrc.domain.Nino
+import uk.gov.hmrc.http.HeaderCarrier
+import v1.controllers.EndpointLogContext
+import v1.mocks.connectors.MockIgnoreEmploymentExpensesConnector
+import v1.models.errors.{DesErrorCode, DesErrors, DownstreamError, ErrorWrapper, MtdError, NinoFormatError, NotFoundError, RuleTaxYearNotEndedError}
+import v1.models.outcomes.ResponseWrapper
+import v1.models.request.ignoreEmploymentExpenses.{IgnoreEmploymentExpensesBody, IgnoreEmploymentExpensesRequest}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+class IgnoreEmploymentExpensesServiceSpec extends UnitSpec {
+
+  val taxYear = "2021-22"
+  val nino = Nino("AA123456A")
+  private val correlationId = "X-123"
+
+  val body = IgnoreEmploymentExpensesBody(true)
+
+  private val requestData = IgnoreEmploymentExpensesRequest(nino, taxYear, body)
+
+  trait Test extends MockIgnoreEmploymentExpensesConnector {
+    implicit val hc: HeaderCarrier = HeaderCarrier()
+    implicit val logContext: EndpointLogContext = EndpointLogContext("c", "ep")
+
+    val service = new IgnoreEmploymentExpensesService(
+      connector = mockIgnoreEmploymentExpensesConnector
+    )
+  }
+
+  "service" should {
+    "service call successful" when {
+      "return mapped result" in new Test {
+        MockIgnoreEmploymentExpensesConnector.ignore(requestData)
+          .returns(Future.successful(Right(ResponseWrapper(correlationId, ()))))
+
+        await(service.amend(requestData)) shouldBe Right(ResponseWrapper(correlationId, ()))
+      }
+    }
+  }
+
+  "unsuccessful" should {
+    "map errors according to spec" when {
+
+      def serviceError(desErrorCode: String, error: MtdError): Unit =
+        s"a $desErrorCode error is returned from the service" in new Test {
+
+          MockIgnoreEmploymentExpensesConnector.ignore(requestData)
+            .returns(Future.successful(Left(ResponseWrapper(correlationId, DesErrors.single(DesErrorCode(desErrorCode))))))
+
+          await(service.amend(requestData)) shouldBe Left(ErrorWrapper(Some(correlationId), error))
+        }
+
+      val input = Seq(
+        "INVALID_TAXABLE_ENTITY_ID" -> NinoFormatError,
+        "INVALID_TAX_YEAR" -> NotFoundError,
+        "INVALID_CORRELATIONID" -> DownstreamError,
+        "INVALID_PAYLOAD" -> DownstreamError,
+        "INVALID_REQUEST_BEFORE_TAX_YEAR_END" -> RuleTaxYearNotEndedError,
+        "SERVER_ERROR" -> DownstreamError,
+        "SERVICE_UNAVAILABLE" -> DownstreamError
+      )
+
+      input.foreach(args => (serviceError _).tupled(args))
+    }
+  }
+}
