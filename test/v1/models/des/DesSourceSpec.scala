a5fd54a4f585a3e60c78491e1e9d009e808ec87f
Adrian Jones <14924378+Nboaram@users.noreply.github.com>
2020-08-05 09:41:50 +0100

    [MTDSA-6581] Retrieve Employment Expenses (#37)
    * MTDSA-6581 Retrieve Employment Expenses Models
    
    * MTDSA-6581 Source Enums
    
    * MTDSA-6581 Source Type and Tests
    
    * MTDSA-6581 Source Type in RequestData
    
    * MTDSA-6581 Enum Tests
    
    * MTDSA-6581 Altered Enums
    
    * MTDSA-6581 Round Trip Tests
    
    * MTDSA-6581 One to One Mapping Removed

---
+++ test/v1/models/des/DesSourceSpec.scala
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package v1.models.des
+
+import support.UnitSpec
+import utils.enums.EnumJsonSpecSupport
+import v1.models.domain.MtdSource
+
+class DesSourceSpec extends UnitSpec with EnumJsonSpecSupport{
+
+  val mtdLatest = MtdSource.`latest`
+  val mtdCustomer = MtdSource.`user`
+  val mtdHmrc = MtdSource.`hmrcHeld`
+
+  val desLatest = DesSource.`LATEST`
+  val desCustomer = DesSource.`CUSTOMER`
+  val desHmrc = DesSource.`HMRC HELD`
+  val desOutgoingHmrc = DesSource.`HMRC-HELD`
+
+  testRoundTrip[DesSource](
+    ("LATEST",DesSource.`LATEST`),
+    ("CUSTOMER",DesSource.`CUSTOMER`),
+    ("HMRC HELD", DesSource.`HMRC HELD`),
+    ("HMRC-HELD", DesSource.`HMRC-HELD`)
+  )
+
+  "mtdSources" when {
+    "when using the toDes" should {
+      "return the correct DesSource for latest" in {
+        desLatest.toMtd shouldBe mtdLatest
+      }
+      "return the correct DesSource for customer" in {
+        desCustomer.toMtd shouldBe mtdCustomer
+      }
+      "return the correct DesSource for hmrcHeld" in {
+        desHmrc.toMtd shouldBe mtdHmrc
+      }
+      "return the correct DesSource for outgoingHmrcHeld" in {
+        desOutgoingHmrc.toMtd shouldBe mtdHmrc
+      }
+    }
+  }
+}
