5689a2e3a64183cff51d5bd7d363358533e33d84
sean-flynn-HMRC <106095575+sean-flynn-HMRC@users.noreply.github.com>
2022-10-20 09:17:02 +0100

    MTDSA-13036: Updated TAX_YEAR_NOT_SUPPORTED error and raml

--- app/v1/models/errors/mtdErrors.scala
+++ app/v1/models/errors/mtdErrors.scala
@@ -42,7 +42,7 @@ object SourceFormatError            extends MtdError("FORMAT_SOURCE", "The provi
 object RuleTaxYearNotSupportedError
     extends MtdError(
       "RULE_TAX_YEAR_NOT_SUPPORTED",
-      "The specified tax year is not supported. That is, the tax year specified is before the minimum tax year value")
+      "The tax year specified does not lie within the supported range")
 
 object RuleIncorrectOrEmptyBodyError extends MtdError("RULE_INCORRECT_OR_EMPTY_BODY_SUBMITTED", "An empty or non-matching body was submitted")
 
