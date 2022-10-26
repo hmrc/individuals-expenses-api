8e73b0cde8db8a9ab2d20e679ba15e64eabb3ddf
Samuel Jacobs <108878134+SamuelJacobsHMRC@users.noreply.github.com>
2022-10-26 11:11:13 +0100

    MTDSA-13045: Updated integration spec to include TYS tests

--- it/v1/endpoints/DeleteEmploymentExpensesControllerISpec.scala
+++ it/v1/endpoints/DeleteEmploymentExpensesControllerISpec.scala
@@ -18,7 +18,7 @@ package v1.endpoints
 
 import com.github.tomakehurst.wiremock.stubbing.StubMapping
 import play.api.http.HeaderNames.ACCEPT
-import play.api.http.Status
+import play.api.http.Status._
 import play.api.libs.json.{JsObject, Json}
 import play.api.libs.ws.{WSRequest, WSResponse}
 import play.api.test.Helpers.AUTHORIZATION
@@ -29,15 +29,13 @@ import v1.stubs.{AuditStub, AuthStub, DownstreamStub, MtdIdLookupStub}
 class DeleteEmploymentExpensesControllerISpec extends IntegrationBaseSpec {
 
   private trait Test {
+    def taxYear: String
+    def setupStubs(): StubMapping
 
-    val nino    = "AA123456A"
-    val taxYear = "2021-22"
+    def nino: String = "AA123456A"
 
     def uri: String = s"/employments/$nino/$taxYear"
-
-    def desUri: String = s"/income-tax/expenses/employments/$nino/$taxYear"
-
-    def setupStubs(): StubMapping
+    def downstreamUri: String = s"/income-tax/expenses/employments/$nino/$taxYear"
 
     def request(): WSRequest = {
       setupStubs()
@@ -48,31 +46,48 @@ class DeleteEmploymentExpensesControllerISpec extends IntegrationBaseSpec {
         )
     }
 
-    def errorBody(code: String): String =
-      s"""
-         |      {
-         |        "code": "$code",
-         |        "reason": "des message"
-         |      }
-    """.stripMargin
-
   }
 
-  "calling the delete endpoint" should {
+  private trait NonTysTest extends Test {
+    override def taxYear: String = "2021-22"
+  }
 
+  private trait TysIfsTest extends Test {
+    val tysYear: String = "23-24"
+    override def taxYear: String = "2023-24"
+
+    override def downstreamUri: String = s"/income-tax/expenses/employments/$tysYear/$nino"
+  }
+
+  "calling the delete employment expenses endpoint" should {
     "return a 204 status" when {
-
-      "any valid request is made" in new Test {
+      "any valid non-TYS request is made" in new NonTysTest {
 
         override def setupStubs(): StubMapping = {
           AuditStub.audit()
           AuthStub.authorised()
           MtdIdLookupStub.ninoFound(nino)
-          DownstreamStub.onSuccess(DownstreamStub.DELETE, desUri, Status.NO_CONTENT, JsObject.empty)
+          DownstreamStub.onSuccess(DownstreamStub.DELETE, downstreamUri, NO_CONTENT, JsObject.empty)
         }
 
         val response: WSResponse = await(request().delete())
-        response.status shouldBe Status.NO_CONTENT
+        response.status shouldBe NO_CONTENT
+        response.body shouldBe ""
+        response.header("X-CorrelationId").nonEmpty shouldBe true
+      }
+
+      "any valid TYS request is made" in new TysIfsTest {
+
+        override def setupStubs(): StubMapping = {
+          AuditStub.audit()
+          AuthStub.authorised()
+          MtdIdLookupStub.ninoFound(nino)
+          DownstreamStub.onSuccess(DownstreamStub.DELETE, downstreamUri, NO_CONTENT, JsObject.empty)
+        }
+
+        val response: WSResponse = await(request().delete())
+        response.status shouldBe NO_CONTENT
+        response.body shouldBe ""
         response.header("X-CorrelationId").nonEmpty shouldBe true
       }
     }
@@ -80,11 +95,15 @@ class DeleteEmploymentExpensesControllerISpec extends IntegrationBaseSpec {
     "return error according to spec" when {
 
       "validation error" when {
-        def validationErrorTest(requestNino: String, requestTaxYear: String, expectedStatus: Int, expectedBody: MtdError): Unit = {
-          s"validation fails with ${expectedBody.code} error" in new Test {
+        def validationErrorTest(requestNino: String,
+                                requestTaxYear: String,
+                                expectedStatus: Int,
+                                expectedBody: MtdError): Unit = {
 
-            override val nino: String    = requestNino
-            override val taxYear: String = requestTaxYear
+          s"validation fails with ${expectedBody.code} error" in new NonTysTest {
+
+            override def nino: String    = requestNino
+            override def taxYear: String = requestTaxYear
 
             override def setupStubs(): StubMapping = {
               AuditStub.audit()
@@ -100,10 +119,10 @@ class DeleteEmploymentExpensesControllerISpec extends IntegrationBaseSpec {
         }
 
         val input = Seq(
-          ("Walrus", "2019-20", Status.BAD_REQUEST, NinoFormatError),
-          ("AA123456A", "203100", Status.BAD_REQUEST, TaxYearFormatError),
-          ("AA123456A", "2018-20", Status.BAD_REQUEST, RuleTaxYearRangeInvalidError),
-          ("AA123456A", "2018-19", Status.BAD_REQUEST, RuleTaxYearNotSupportedError)
+          ("Walrus", "2019-20", BAD_REQUEST, NinoFormatError),
+          ("AA123456A", "203100", BAD_REQUEST, TaxYearFormatError),
+          ("AA123456A", "2018-20", BAD_REQUEST, RuleTaxYearRangeInvalidError),
+          ("AA123456A", "2018-19", BAD_REQUEST, RuleTaxYearNotSupportedError)
         )
 
         input.foreach(args => (validationErrorTest _).tupled(args))
@@ -111,13 +130,13 @@ class DeleteEmploymentExpensesControllerISpec extends IntegrationBaseSpec {
 
       "des service error" when {
         def serviceErrorTest(desStatus: Int, desCode: String, expectedStatus: Int, expectedBody: MtdError): Unit = {
-          s"des returns an $desCode error and status $desStatus" in new Test {
+          s"des returns an $desCode error and status $desStatus" in new NonTysTest {
 
             override def setupStubs(): StubMapping = {
               AuditStub.audit()
               AuthStub.authorised()
               MtdIdLookupStub.ninoFound(nino)
-              DownstreamStub.onError(DownstreamStub.DELETE, desUri, desStatus, errorBody(desCode))
+              DownstreamStub.onError(DownstreamStub.DELETE, downstreamUri, desStatus, errorBody(desCode))
             }
 
             val response: WSResponse = await(request().delete())
@@ -126,16 +145,32 @@ class DeleteEmploymentExpensesControllerISpec extends IntegrationBaseSpec {
           }
         }
 
-        val input = Seq(
-          (Status.BAD_REQUEST, "INVALID_TAXABLE_ENTITY_ID", Status.BAD_REQUEST, NinoFormatError),
-          (Status.BAD_REQUEST, "INVALID_TAX_YEAR", Status.BAD_REQUEST, TaxYearFormatError),
-          (Status.BAD_REQUEST, "INVALID_CORRELATIONID", Status.INTERNAL_SERVER_ERROR, StandardDownstreamError),
-          (Status.NOT_FOUND, "NO_DATA_FOUND", Status.NOT_FOUND, NotFoundError),
-          (Status.INTERNAL_SERVER_ERROR, "SERVER_ERROR", Status.INTERNAL_SERVER_ERROR, StandardDownstreamError),
-          (Status.SERVICE_UNAVAILABLE, "SERVICE_UNAVAILABLE", Status.INTERNAL_SERVER_ERROR, StandardDownstreamError)
+        def errorBody(code: String): String =
+          s"""
+             |      {
+             |        "code": "$code",
+             |        "reason": "des message"
+             |      }
+    """.stripMargin
+
+        val errors = Seq(
+          (BAD_REQUEST, "INVALID_TAXABLE_ENTITY_ID", BAD_REQUEST, NinoFormatError),
+          (BAD_REQUEST, "INVALID_TAX_YEAR", BAD_REQUEST, TaxYearFormatError),
+          (BAD_REQUEST, "INVALID_CORRELATIONID", INTERNAL_SERVER_ERROR, StandardDownstreamError),
+          (NOT_FOUND, "NO_DATA_FOUND", NOT_FOUND, NotFoundError),
+          (INTERNAL_SERVER_ERROR, "SERVER_ERROR", INTERNAL_SERVER_ERROR, StandardDownstreamError),
+          (SERVICE_UNAVAILABLE, "SERVICE_UNAVAILABLE", INTERNAL_SERVER_ERROR, StandardDownstreamError)
         )
 
-        input.foreach(args => (serviceErrorTest _).tupled(args))
+        val extraTysErrors = Seq(
+          (BAD_REQUEST, "INVALID_CORRELATION_ID", INTERNAL_SERVER_ERROR, StandardDownstreamError),
+          (NOT_FOUND, "NOT_FOUND", NOT_FOUND, NotFoundError),
+          (UNPROCESSABLE_ENTITY, "TAX_YEAR_NOT_SUPPORTED", BAD_REQUEST, RuleTaxYearNotSupportedError)
+        )
+
+
+
+        (errors ++ extraTysErrors).foreach(args => (serviceErrorTest _).tupled(args))
       }
     }
   }
