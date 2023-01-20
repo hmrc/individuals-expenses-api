3940c473b4cb072be2abf7dca8e97546c8c13438
Matt Stephens <matthew.stephens@digital.hmrc.gov.uk>
2023-01-20 16:22:19 +0000

    MTDSA-12712 OAS feature switch spike

--- it/support/IntegrationBaseSpec.scala
+++ it/support/IntegrationBaseSpec.scala
@@ -41,7 +41,6 @@ trait IntegrationBaseSpec extends UnitSpec with WireMockHelper with GuiceOneServ
     "microservice.services.ifsR6.port"         -> mockPort,
     "microservice.services.tys-ifs.host"       -> mockHost,
     "microservice.services.tys-ifs.port"       -> mockPort,
-    "microservice.services.openApiTest"       -> mockPort,
     "microservice.services.mtd-id-lookup.host" -> mockHost,
     "microservice.services.mtd-id-lookup.port" -> mockPort,
     "microservice.services.auth.host"          -> mockHost,
