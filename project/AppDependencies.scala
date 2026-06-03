3476580ef703fba3b35ac59213d39a8da64b6b5b
robomancap <182665093+robomancap@users.noreply.github.com>
2026-06-03 15:29:00 +0100

    Upgrade handlebar and other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,24 +19,24 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapPlayVersion = "9.19.0"
+  val bootstrapPlayVersion = "10.7.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-backend-play-30" % bootstrapPlayVersion,
     "org.typelevel"                 %% "cats-core"                 % "2.13.0",
     "com.neovisionaries"            % "nv-i18n"                    % "1.29",
-    "com.github.jknack"             % "handlebars"                 % "4.3.1"
+    "com.github.jknack"             % "handlebars"                 % "4.5.1"
   )
 
   val test: Seq[sbt.ModuleID] = Seq(
     "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootstrapPlayVersion,
-    "org.scalamock"       %% "scalamock"              % "7.5.0",
-    "org.scalatestplus"   %% "scalacheck-1-18"        % "3.2.19.0"
+    "org.scalamock"       %% "scalamock"              % "7.5.5",
+    "org.scalatestplus"   %% "scalacheck-1-19"        % "3.2.20.0"
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = Seq(
-    "io.swagger.parser.v3" % "swagger-parser-v3"      % "2.1.34",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.20.0"
+    "io.swagger.parser.v3" % "swagger-parser-v3"      % "2.1.43",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.21.4"
   ).map(_ % Test)
 
 
